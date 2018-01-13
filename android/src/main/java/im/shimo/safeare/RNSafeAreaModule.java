c86c8749d0463a93f6bb3922624921418dfd8158
wsong910 <wsong910@users.noreply.github.com>
2018-01-13 19:18:31 +0800

    Update RNSafeAreaModule.java

--- android/src/main/java/im/shimo/safeare/RNSafeAreaModule.java
+++ android/src/main/java/im/shimo/safeare/RNSafeAreaModule.java
@@ -1,26 +1,32 @@
 
+
 package im.shimo.safeare;
 
 
 import android.content.res.Resources;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
 
 public class RNSafeAreaModule extends ReactContextBaseJavaModule {
     private static int mNavigationBarHeight = 0;
-
-    static {
-        Resources resources = Resources.getSystem();
-        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            mNavigationBarHeight = resources.getDimensionPixelSize(resourceId);
-        }
-    }
+    private static int mStatusBarHeight = 25;
 
     public RNSafeAreaModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        Resources resources = reactContext.getResources();
+        int bottomId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (bottomId > 0) {
+            mNavigationBarHeight = resources.getDimensionPixelSize(bottomId);
+        }
+        int topId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        if (topId > 0) {
+            mStatusBarHeight = resources.getDimensionPixelSize(topId);
+        }
     }
 
     @Override
@@ -29,8 +35,14 @@ public class RNSafeAreaModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public int getRootSafeArea() {
-        return mNavigationBarHeight;
+    public void getRootSafeArea(Promise promise) {
+        WritableMap result = Arguments.createMap();
+        result.putInt("top", mStatusBarHeight);
+        result.putInt("bottom", mNavigationBarHeight);
+        result.putInt("left", 0);
+        result.putInt("right", 0);
+        promise.resolve(result);
     }
 
 }
+
